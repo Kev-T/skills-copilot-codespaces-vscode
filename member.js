db9df695ba9c7e28d255c144869c7f9952f6e20e
KT-Dev <kevin.taupin@sogeti.com>
2024-12-11 12:23:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    members: [
+      { name: 'John', skills: ['javascript', 'css', 'html'] },
+      { name: 'Jane', skills: ['javascript', 'css', 'html'] },
+      { name: 'Jim', skills: ['javascript', 'css', 'html'] },
+    ],
+  };
+}
